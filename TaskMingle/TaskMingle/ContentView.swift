363602276bbcff451f99fdbfac5e1a58d99c9223
Aran Ali <Aliaran2717@gmail.com>
2024-02-18 19:34:11 +0100

    test commit

---
+++ TaskMingle/TaskMingle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskMingle
+//
+//  Created by Aran Ali on 2024-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskMingle/TaskMingle/TaskMingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMingleApp.swift
+//  TaskMingle
+//
+//  Created by Aran Ali on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct TaskMingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
