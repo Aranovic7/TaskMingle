089919569343d50323bd26c357dc2a1b530e750d
Aran Ali <Aliaran2717@gmail.com>
2024-02-19 20:02:13 +0100

    added MainGoalScreen

---
+++ TaskMingle/TaskMingle/Screens/MainGoalScreen.swift
@@ -0,0 +1,105 @@
+//
+//  MainGoalScreen.swift
+//  TaskMingle
+//
+//  Created by Aran Ali on 2024-02-19.
+//
+
+import SwiftUI
+
+struct MainGoalScreen: View {
+    var body: some View {
+        VStack{
+            
+            ZStack{
+                
+                Color(red: 38/255, green: 170/255, blue: 197/255)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    
+                    Text("What is your main goal right now?")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                        .padding()
+                    
+                    Text("Please, choose one or more main goals. We'll provide you with recommendations.")
+                        .foregroundStyle(.white)
+                        .font(.title3)
+                        .bold()
+                        .padding()
+                    
+                    Button(action: {
+                        print("button was pressed")
+                    }, label: {
+                        Text("Increase productivity")
+                            .foregroundStyle(Color(red: 21/255, green: 107/255, blue: 171/255))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .frame(width: 250, height: 50)
+                            .foregroundStyle(.white)
+                            .background(Color.white)
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(.top, 20)
+                    })
+                    
+                    Button(action: {
+                        print("button was pressed")
+                    }, label: {
+                        Text("Reduce stress")
+                            .foregroundStyle(Color(red: 21/255, green: 107/255, blue: 171/255))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .frame(width: 250, height: 50)
+                            .foregroundStyle(.white)
+                            .background(Color.white)
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(.top, 20)
+                    })
+                    
+                    Button(action: {
+                        print("button was pressed")
+                    }, label: {
+                        Text("Become more disciplined")
+                            .foregroundStyle(Color(red: 21/255, green: 107/255, blue: 171/255))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .frame(width: 250, height: 50)
+                            .foregroundStyle(.white)
+                            .background(Color.white)
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(.top, 20)
+                    })
+                    
+                    Button(action: {
+                        print("button was pressed")
+                    }, label: {
+                        Text("Sleep better")
+                            .foregroundStyle(Color(red: 21/255, green: 107/255, blue: 171/255))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .frame(width: 250, height: 50)
+                            .foregroundStyle(.white)
+                            .background(Color.white)
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(.top, 20)
+                    })
+                    
+                    
+                    
+                    Spacer()
+                }
+
+            }
+        }
+    }
+}
+
+#Preview {
+    MainGoalScreen()
+}
--- TaskMingle/TaskMingle/Screens/StartScreen.swift
+++ TaskMingle/TaskMingle/Screens/StartScreen.swift
@@ -9,27 +9,32 @@ import SwiftUI
 
 struct SplashScreen: View {
     var body: some View {
+        
         VStack{
             ZStack(alignment: .topLeading){
                     Image("blueCircle2")
                     Image("blueCircle")
-                
-                
             }
             .offset(x: -102)
             .ignoresSafeArea()
+            
+            
+            
             Spacer()
+            
             VStack{
+                
                 Image("loadingIllustration")
                     .padding()
+                
                 Text("Get things done with TaskMingle")
                     .font(.title2)
                     .bold()
                    
                 Text("TaskMingle is an app designed for whoever you are who wants to achive a more peaceful, happy and disciplined life. This app helps you with daily tasks, motivation, and much more thins! ")
                     .padding()
-                
                     .font(.subheadline)
+                
                 Button(action: {
                     print("button was pressed")
                 }, label: {
@@ -42,20 +47,13 @@ struct SplashScreen: View {
                         .background(Color(red: 38/255, green: 170/255, blue: 197/255))
                         .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 0)
                         .clipShape(.rect(cornerRadius: 10))
-                       
-                    
+                        .padding(.top, 20)
                 })
               
-                    
-                    
             }
             .offset(y: -100)
             
-          
             Spacer()
-          
-                
-        
             
         }
     }
