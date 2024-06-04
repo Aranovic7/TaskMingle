c8bae415e8c9a416bd7665ba530f9a4614ba260e
Aranovic7 <aliaran2717@gmail.com>
2024-06-04 21:38:05 +0200

    Deleted StartedScreen and added LandingPage

---
+++ TaskMingle/TaskMingle/Screens/UnAuthenticated/LandingPage.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPage.swift
+//  TaskMingle
+//
+//  Created by Aran Ali on 2024-06-04.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingPage()
+}
